6f9f0c0916e09a4fe1ae681f5fd97e579e8f3ee1
Sandeep325 <negisandeep9819@gmail.com>
2020-09-07 10:15:25 +0530

    Updates

---
+++ precache-manifest.e23c060a3fcfee429a7bf500ede4ae29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662cb3ace7ac101c3248f89fa53cad5f",
+    "url": "/topwebseries_reactjs/index.html"
+  },
+  {
+    "revision": "d1539aa46e8275982836",
+    "url": "/topwebseries_reactjs/static/css/main.3a147cd9.chunk.css"
+  },
+  {
+    "revision": "e7acd39e4efc7eef70cd",
+    "url": "/topwebseries_reactjs/static/js/2.000887fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/topwebseries_reactjs/static/js/2.000887fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1539aa46e8275982836",
+    "url": "/topwebseries_reactjs/static/js/main.a276cfef.chunk.js"
+  },
+  {
+    "revision": "2d608eeb08a10aeee36f",
+    "url": "/topwebseries_reactjs/static/js/runtime-main.fd234bf6.js"
+  }
+]);
